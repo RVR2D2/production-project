39d6e28d58739b4129ce5bbdfb34b206e0a067ff
REDKINVA <59052910+RVR2D2@users.noreply.github.com>
2023-11-23 16:41:38 +0600

    feat: SVG loader, File loader, Button ui kit

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import style from './Navbar.module.scss'
 
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string
@@ -11,6 +12,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(style.Navbar, {}, [className])}>
+      <ThemeSwitcher/>
       <div className={style.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={style.mainLink}>Главная</AppLink>
         <AppLink theme={AppLinkTheme.PRIMARY} to={'/about'}>О сайте</AppLink>
