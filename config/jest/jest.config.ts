53f768f621bfe3a3d15ba167a8c70be795653277
REDKINVA <59052910+RVR2D2@users.noreply.github.com>
2023-11-29 17:17:35 +0600

    feat: add react test library, storybook

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,131 +3,191 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
-  // automock: false,
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: [
+        '\\\\node_modules\\\\',
+    ],
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
+    moduleDirectories: [
+        'node_modules',
+    ],
+    modulePaths: [
+        '<rootDir>src',
+    ],
+    testMatch: [
+        // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
+        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    ],
+    rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
+    // Indicates whether the coverage information should be collected while executing the test
+    // collectCoverage: false,
 
-  // bail: 0,
+    // An array of glob patterns indicating a set of files for which coverage information should be collected
+    // collectCoverageFrom: undefined,
 
-  // cacheDirectory: "C:\\Users\\tim\\AppData\\Local\\Temp\\jest",
+    // The directory where Jest should output its coverage files
+    // coverageDirectory: undefined,
 
-  clearMocks: true,
-  testEnvironment: 'jsdom',
-  coveragePathIgnorePatterns: [
-    '\\\\node_modules\\\\',
-  ],
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'ts',
-    'tsx',
-    'json',
-    'node',
-  ],
-  moduleDirectories: [
-    'node_modules',
-  ],
-  testMatch: [
-    // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
-    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
-  ],
-  rootDir: '../../',
-  // collectCoverage: false,
+    // An array of regexp pattern strings used to skip coverage collection
 
-  // collectCoverageFrom: undefined,
+    // Indicates which provider should be used to instrument code for coverage
+    // coverageProvider: "babel",
 
-  // coverageDirectory: undefined,
+    // A list of reporter names that Jest uses when writing coverage reports
+    // coverageReporters: [
+    //   "json",
+    //   "text",
+    //   "lcov",
+    //   "clover"
+    // ],
 
-  // coverageProvider: "babel",
+    // An object that configures minimum threshold enforcement for coverage results
+    // coverageThreshold: undefined,
 
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+    // A path to a custom dependency extractor
+    // dependencyExtractor: undefined,
 
-  // coverageThreshold: undefined,
+    // Make calling deprecated APIs throw helpful error messages
+    // errorOnDeprecated: false,
 
-  // dependencyExtractor: undefined,
+    // Force coverage collection from ignored files using an array of glob patterns
+    // forceCoverageMatch: [],
 
-  // errorOnDeprecated: false,
+    // A path to a module which exports an async function that is triggered once before all test suites
+    // globalSetup: undefined,
 
-  // forceCoverageMatch: [],
+    // A path to a module which exports an async function that is triggered once after all test suites
+    // globalTeardown: undefined,
 
-  // globalSetup: undefined,
+    // A set of global variables that need to be available in all test environments
+    // globals: {},
 
-  // globalTeardown: undefined,
+    // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+    // maxWorkers: "50%",
 
-  // globals: {},
+    // An array of directory names to be searched recursively up from the requiring module's location
 
-  // maxWorkers: "50%",
+    // An array of file extensions your modules use
 
-  // moduleNameMapper: {},
+    // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+    // moduleNameMapper: {},
 
-  // modulePathIgnorePatterns: [],
+    // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+    // modulePathIgnorePatterns: [],
 
-  // notify: false,
+    // Activates notifications for test results
+    // notify: false,
 
-  // notifyMode: "failure-change",
+    // An enum that specifies notification mode. Requires { notify: true }
+    // notifyMode: "failure-change",
 
-  // preset: undefined,
+    // A preset that is used as a base for Jest's configuration
+    // preset: undefined,
 
-  // projects: undefined,
+    // Run tests from one or more projects
+    // projects: undefined,
 
-  // reporters: undefined,
+    // Use this configuration option to add custom reporters to Jest
+    // reporters: undefined,
 
-  // resetMocks: false,
+    // Automatically reset mock state before every test
+    // resetMocks: false,
 
-  // resetModules: false,
+    // Reset the module registry before running each individual test
+    // resetModules: false,
 
-  // resolver: undefined,
+    // A path to a custom resolver
+    // resolver: undefined,
 
-  // restoreMocks: false,
+    // Automatically restore mock state and implementation before every test
+    // restoreMocks: false,
 
-  // roots: [
-  //   "<rootDir>"
-  // ],
+    // The root directory that Jest should scan for tests and modules within
 
-  // runner: "jest-runner",
+    // A list of paths to directories that Jest should use to search for files in
+    // roots: [
+    //   "<rootDir>"
+    // ],
 
-  // setupFiles: [],
+    // Allows you to use a custom runner instead of Jest's default test runner
+    // runner: "jest-runner",
 
-  // setupFilesAfterEnv: [],
+    // The paths to modules that run some code to configure or set up the testing environment before each test
+    // setupFiles: [],
 
-  // slowTestThreshold: 5,
+    // A list of paths to modules that run some code to configure or set up the testing framework before each test
+    // setupFilesAfterEnv: [],
 
-  // snapshotSerializers: [],
+    // The number of seconds after which a test is considered as slow and reported as such in the results.
+    // slowTestThreshold: 5,
 
-  // testEnvironmentOptions: {},
+    // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+    // snapshotSerializers: [],
 
-  // testLocationInResults: false,
+    // The test environment that will be used for testing
 
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+    // Options that will be passed to the testEnvironment
+    // testEnvironmentOptions: {},
 
-  // testRegex: [],
+    // Adds a location field to test results
+    // testLocationInResults: false,
 
-  // testResultsProcessor: undefined,
+    // The glob patterns Jest uses to detect test files
 
-  // testRunner: "jest-circus/runner",
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    // testPathIgnorePatterns: [
+    //   "\\\\node_modules\\\\"
+    // ],
 
-  // testURL: "http://localhost",
+    // The regexp pattern or array of patterns that Jest uses to detect test files
+    // testRegex: [],
 
-  // timers: "real",
+    // This option allows the use of a custom results processor
+    // testResultsProcessor: undefined,
 
-  // transform: undefined,
+    // This option allows use of a custom test runner
+    // testRunner: "jest-circus/runner",
 
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
+    // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
+    // testURL: "http://localhost",
 
-  // unmockedModulePathPatterns: undefined,
+    // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
+    // timers: "real",
 
-  // verbose: undefined,
+    // A map from regular expressions to paths to transformers
+    // transform: undefined,
 
-  // watchPathIgnorePatterns: [],
+    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+    // transformIgnorePatterns: [
+    //   "\\\\node_modules\\\\",
+    //   "\\.pnp\\.[^\\\\]+$"
+    // ],
 
-  // watchman: true,
+    // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+    // unmockedModulePathPatterns: undefined,
+
+    // Indicates whether each individual test should be reported during the run
+    // verbose: undefined,
+
+    // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+    // watchPathIgnorePatterns: [],
+
+    // Whether to use watchman for file crawling
+    // watchman: true,
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -10,21 +10,27 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(false);
 
-  const onToggle = () => {
-    setCollapsed((prev) => !prev);
-  };
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
 
-  return (
-    <div
-      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
-    >
-      <Button onClick={onToggle}>toggle</Button>
-      <div className={cls.switchers}>
-        <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
-      </div>
-    </div>
-  );
+    return (
+        <div
+            data-testid="sidebar"
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+        >
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                toggle
+            </Button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
+            </div>
+        </div>
+    );
 };
