07f09b754fcd3414c1a8b79f1e7fe63976791b2a
REDKINVA <59052910+RVR2D2@users.noreply.github.com>
2023-12-03 21:43:21 +0600

    feat add redux toolkit, test component, add test slice

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,33 @@
+import {
+    componentRender,
+} from 'shared/lib/tests/componentRender/componentRender';
+import { screen } from '@testing-library/react';
+import { userEvent } from '@storybook/testing-library';
+import { Counter } from './Counter';
+
+describe('Counter component', () => {
+    test('should get value', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+    });
+
+    test('should increment', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+
+        userEvent.click(screen.getByTestId('increment-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+    });
+
+    test('should decrement', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+
+        userEvent.click(screen.getByTestId('decrement-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+    });
+});
